9fc1b3ab0e0e16502b282297f9ef8080d5f275d5
Bram <bram.de.jaegher@gmail.com>
2018-05-02 10:08:31 +0200

    added project solution

--- Chapters/09.NPCompleteProblems/mcp_BDJ.py
+++ Chapters/09.NPCompleteProblems/mcp_BDJ.py
@@ -2,5 +2,4 @@ def c(S,U):
     import itertools as i
     for l in range(len(S)):
         for s in i.combinations(S,l+1):
-            if set.union(*s)==U:
-                return s
+            if set.union(*s)==U:return s
